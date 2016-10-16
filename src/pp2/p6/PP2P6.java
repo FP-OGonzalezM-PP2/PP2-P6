eb2be24538e17950021006ffebea39d4824ec658
Osvaldo <Osvaldo@Osvaldo>
2016-10-15 23:29:56 -0500

    Horas de trabajo

--- src/pp2/p6/PP2P6.java
+++ src/pp2/p6/PP2P6.java
@@ -17,6 +17,7 @@ public class PP2P6 {
     public static void main(String[] args) {
         // TODO code application logic here
         double h, s, p;
+        //en h se almacenara el numero de horas trabajadas y en s el valor de cada hora de trabajo
         h = pedirHoras();
         s = pedirSalario();
         p = Salario(h,s);
@@ -38,11 +39,14 @@ public class PP2P6 {
     }
     public static double Salario(double h, double s){
         double p=0;
+        //si son menos de 48 horas de trabajo se multiplica solo por el valor
         if(h<=48){
             p=h*s;
         }if(h>48 && h<=56){
+            // si se pasa de las 48 horas pero son menos 8 horas, las horas excedentes se le restan a las 48 para multiplicarlas
             p =((h-48)*(2*s))+(48*s);
         }if(h>56){
+            // si pasan de las 8 extras, las exedentes se le restan a las 56, para multiplicarlas
             p = (h-56)*(3*s)+(8*(2*s))+(48*s);
         }
         return p;
